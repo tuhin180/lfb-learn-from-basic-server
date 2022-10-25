45d2594990f02015a31e5014d702ea10efc7b659
tuhin180 <tanjirtuhin18@gmail.com>
2022-10-25 22:02:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+const port = process.env.port || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Now server is running");
+});
+app.listen(port, () => {
+  console.log("server is running ", port);
+});
