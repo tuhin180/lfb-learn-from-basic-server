27a8348a606eea6483de871849cebc41928c80c2
tuhin180 <tanjirtuhin18@gmail.com>
2022-10-26 22:03:22 +0600

    first

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(cors());
 const port = process.env.port || 5000;
 
 const courses = require("./data/courses.json");
+const courseDetails = require("./data/course-details.json");
 
 app.get("/", (req, res) => {
   res.send("Now server is running");
@@ -13,7 +14,18 @@ app.get("/", (req, res) => {
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
-
+app.get("/course", (req, res) => {
+  res.send(courseDetails);
+});
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const getSingleCourse = courseDetails?.find((c) => c.id == id);
+  if (!getSingleCourse) {
+    res.send("course not available");
+  } else {
+    res.send(getSingleCourse);
+  }
+});
 app.listen(port, () => {
   console.log("server is running ", port);
 });
